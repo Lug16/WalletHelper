f5ac9887b6de12b65d75f03e5ff98b174c07efa5
Lug Velez Schmitz <lugvelez@hotmail.com>
2013-11-14 01:15:08 -0500

    Renamed Projects for standarization, and basic entities

--- WalletHelper.DataAccess/Class1.cs
+++ WalletHelper.DataAccess/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace clWalletHelper
+namespace WalletHelper.DataAccess
 {
-    public class Class12
+    public class Class1
     {
     }
 }
